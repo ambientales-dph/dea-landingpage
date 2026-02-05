4c05c1516400d4bd7b519596fcf615deddfcd7f7
Estudios Ambientales DPH <ambientales.dph@gmail.com>
2026-02-05 17:38:10 +0000

    Bien! Recordemos poner la portada de la tarjeta en rojo cuando la creemo

--- src/lib/cuencas.ts
+++ src/lib/cuencas.ts
@@ -1,15 +1,23 @@
 // TODO: Completa esta lista con todas las cuencas y sus datos correspondientes.
 // El `trelloListName` debe coincidir EXACTAMENTE con el nombre de la lista en tu tablero de Trello "Proyectos".
 export const CUENCAS = [
-  { id: 'matanza_riachuelo', name: 'Cuenca Matanza Riachuelo', code: 'CMR', trelloListName: 'Matanza Riachuelo' },
-  { id: 'reconquista', name: 'Cuenca del Reconquista', code: 'RCR', trelloListName: 'Reconquista' },
-  { id: 'lujan', name: 'Cuenca del Río Luján', code: 'RLJ', trelloListName: 'Luján' },
-  { id: 'salado', name: 'Cuenca del Salado', code: 'RSA', trelloListName: 'Salado' },
-  { id: 'areco_arrecifes', name: 'Cuenca de los ríos Areco y Arrecifes', code: 'RAA', trelloListName: 'Areco-Arrecifes' },
-  { id: 'arroyos_sur', name: 'Arroyos del Sur', code: 'ADS', trelloListName: 'Arroyos del Sur' },
-  { id: 'reg_atlantica', name: 'Región Atlántica', code: 'RAT', trelloListName: 'Región Atlántica' },
-  { id: 'delta', name: 'Delta del Paraná', code: 'DPP', trelloListName: 'Delta' },
-  { id: 'otros', name: 'Otros', code: 'OTR', trelloListName: 'Otros' },
+  { id: 'mar', name: 'Cuenca Matanza-Riachuelo', code: 'MAR', trelloListName: 'Cuenca Matanza-Riachuelo' },
+  { id: 'rrq', name: 'Cuenca Río Reconquista', code: 'RRQ', trelloListName: 'Cuenca Río Reconquista' },
+  { id: 'rlu', name: 'Cuenca Luján', code: 'RLU', trelloListName: 'Cuenca Luján' },
+  { id: 'rsa', name: 'Cuenca del Río Salado', code: 'RSA', trelloListName: 'Cuenca del Río Salado' },
+  { id: 'arr', name: 'Cuenca Rio Arrecifes', code: 'ARR', trelloListName: 'Cuenca Rio Arrecifes' },
+  { id: 'vae', name: 'Vertiente Atlántica Este', code: 'VAE', trelloListName: 'Vertiente Atlántica Este' },
+  { id: 'rpm', name: 'Vertiente Río de la Plata Intermedia', code: 'RPM', trelloListName: 'Vertiente Río de la Plata Intermedia' },
+  { id: 'rps', name: 'Vertiente Río de la Plata Superior', code: 'RPS', trelloListName: 'Vertiente Río de la Plata Superior' },
+  { id: 'rar', name: 'Cuenca Río Areco', code: 'RAR', trelloListName: 'Cuenca Río Areco' },
+  { id: 'cm', name: 'Canales de Marea', code: 'CM', trelloListName: 'Canales de Marea' },
+  { id: 'rco', name: 'Cuenca Río Colorado', code: 'RCO', trelloListName: 'Cuenca Río Colorado' },
+  { id: 'vas', name: 'Vertiente Atlantica Sur', code: 'VAS', trelloListName: 'Vertiente Atlantica Sur' },
+  { id: 'crz', name: 'Cuenca Arroyo de la Cruz', code: 'CRZ', trelloListName: 'Cuenca Arroyo de la Cruz' },
+  { id: 'qqg', name: 'Cuenca Quequén Grande', code: 'QQG', trelloListName: 'Cuenca Quequén Grande' },
+  { id: 'sn', name: 'Cuenca S/N', code: 'SN', trelloListName: 'Cuenca S/N' },
+  { id: 'rng', name: 'Cuenca de Río Negro', code: 'RNG', trelloListName: 'Cuenca de Río Negro' },
+  { id: 'rpi', name: 'Vertiente Río de La Plata Inferior', code: 'RPI', trelloListName: 'Vertiente Río de La Plata Inferior' },
 ];
 
 export const DESCRIPCION_PLANTILLA = `PARTIDO: 
