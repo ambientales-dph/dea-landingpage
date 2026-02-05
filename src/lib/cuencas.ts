19c715a219d1f0efb44da693c97f47d81a7cb101
Estudios Ambientales DPH <ambientales.dph@gmail.com>
2026-02-05 17:02:46 +0000

    Debemos importar la lista de listas del tablero de Proyectos de Trello p

--- src/lib/cuencas.ts
+++ src/lib/cuencas.ts
@@ -1,10 +1,15 @@
 // TODO: Completa esta lista con todas las cuencas y sus datos correspondientes.
 // El `trelloListName` debe coincidir EXACTAMENTE con el nombre de la lista en tu tablero de Trello "Proyectos".
 export const CUENCAS = [
-  { id: 'salado', name: 'Cuenca del Salado', code: 'RSA', trelloListName: 'Salado' },
+  { id: 'matanza_riachuelo', name: 'Cuenca Matanza Riachuelo', code: 'CMR', trelloListName: 'Matanza Riachuelo' },
   { id: 'reconquista', name: 'Cuenca del Reconquista', code: 'RCR', trelloListName: 'Reconquista' },
-  { id: 'lujan', name: 'Cuenca del Luján', code: 'RLJ', trelloListName: 'Lujan' },
-  // Agrega aquí el resto de las cuencas con sus códigos y nombres de lista en Trello
+  { id: 'lujan', name: 'Cuenca del Río Luján', code: 'RLJ', trelloListName: 'Luján' },
+  { id: 'salado', name: 'Cuenca del Salado', code: 'RSA', trelloListName: 'Salado' },
+  { id: 'areco_arrecifes', name: 'Cuenca de los ríos Areco y Arrecifes', code: 'RAA', trelloListName: 'Areco-Arrecifes' },
+  { id: 'arroyos_sur', name: 'Arroyos del Sur', code: 'ADS', trelloListName: 'Arroyos del Sur' },
+  { id: 'reg_atlantica', name: 'Región Atlántica', code: 'RAT', trelloListName: 'Región Atlántica' },
+  { id: 'delta', name: 'Delta del Paraná', code: 'DPP', trelloListName: 'Delta' },
+  { id: 'otros', name: 'Otros', code: 'OTR', trelloListName: 'Otros' },
 ];
 
 export const DESCRIPCION_PLANTILLA = `PARTIDO: 
