168b53f426756fdcdab2d978fbef3170e8197db2
Estudios Ambientales DPH <ambientales.dph@gmail.com>
2026-02-18 14:42:41 +0000

    Cómo cambio el favicon de esta página?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: "DEA",
   description: "Departamento de Estudios Ambientales",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
