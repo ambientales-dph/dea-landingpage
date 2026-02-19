a50e3443a0803a107f3c877bb677c524b8d12c0e
Estudios Ambientales DPH <ambientales.dph@gmail.com>
2026-02-19 23:55:53 +0000

    SNRD no me está encontrando coincidencias con búsquedas sencillas que yo

--- src/services/snrd.ts
+++ src/services/snrd.ts
@@ -9,7 +9,7 @@ export interface SNRDArticle {
 }
 
 export async function searchSNRD(query: string): Promise<SNRDArticle[]> {
-  const url = `https://bdu.siu.edu.ar/busqueda/inicio/ajax?query=${encodeURIComponent(query)}&page=1&sort_by=score&order=desc&rpp=10&log=true&filter=~resource_type_str:article,book,book_part,technical_report,thesis`;
+  const url = `https://bdu.siu.edu.ar/busqueda/inicio/ajax?query=${encodeURIComponent(query)}&page=1&sort_by=score&order=desc&rpp=10`;
 
   try {
     const fetch = (await import('node-fetch')).default;
